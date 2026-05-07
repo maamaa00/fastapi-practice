d4f6d6d7679a1a01785c441559bd3325230b1953
maamaa00 <maamaa00@users.noreply.github.com>
2026-05-07 23:27:00 +0900

    feat: メモAPIにID管理を追加

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ app = FastAPI()
 memos = []
 
 class Memo(BaseModel):
+    id: int
     text: str
+
+class MemoCreate(BaseModel):
+    text: str
+
+next_id = 0
 
 @app.get("/")
 def read_root():
@@ -18,21 +24,22 @@ def get_memos():
     return memos
 
 @app.post("/memos")
-def create_memo(memo: Memo):
-    memos.append(memo)
-    return memo
+def create_memo(memo: MemoCreate):
+    global next_id
 
-# 最低限
-# @app.delete("/memos/{memo_id}")
-# def delete_memo(memo_id: int):
-#     delete_memo = memos.pop(memo_id)
-#     return delete_memo
+    new_memo = {
+        "id": next_id,
+        "text": memo.text
+    }
 
-# 存在チェック
+    memos.append(new_memo)
+    next_id += 1
+    return new_memo
+
 @app.delete("/memos/{memo_id}")
 def delete_memo(memo_id: int):
-    if memo_id >= len(memos):
-        raise HTTPException(status_code=404, detail="Memo not found")
-    
-    delete_memo = memos.pop(memo_id)
-    return delete_memo
+    for i, memo in enumerate(memos):
+        if memo["id"] == memo_id:
+            return memos.pop(i)
+
+    raise HTTPException(status_code=404, detail="Memo not found")
